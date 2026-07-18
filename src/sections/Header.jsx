cddae5bc4ff812eeff0b03003b2ebf2065703beb
ali404a <alialmortda@gmail.com>
2026-07-18 16:28:08 +0300

    Fix: slim header with properly sized logo (52px height, compact padding)

--- src/sections/Header.jsx
+++ src/sections/Header.jsx
@@ -21,7 +21,7 @@ const Header = () => {
       <div className="container header-inner">
         
         <Link to="/" className="header-logo">
-          <img src={logoWhite} alt={t("مرماز أكاديمي")} style={{ height: '110px', objectFit: 'contain', maxHeight: 'none' }} />
+          <img src={logoWhite} alt={t("مرماز أكاديمي")} />
         </Link>
 
         <nav className="header-nav">
